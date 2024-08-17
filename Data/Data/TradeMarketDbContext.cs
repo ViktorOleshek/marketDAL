5a345e8b1e1a8ed6bbd06ff7c09712bd168af4a4
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-08-18 01:07:58 +0300

    Implement TradeMarketDBContext with DbSet and Fluent API config

--- Data/Data/TradeMarketDbContext.cs
+++ Data/Data/TradeMarketDbContext.cs
@@ -1,16 +1,94 @@
 ﻿using Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Data.Data
 {
     public class TradeMarketDbContext : DbContext
     {
+        public TradeMarketDbContext()
+        {
+
+        }
+
         public TradeMarketDbContext(DbContextOptions<TradeMarketDbContext> options) : base(options)
         {
         }
-        //TODO: write DbSets for entities
 
-        //TODO: write Fluent API configuration
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<ReceiptDetail> ReceiptsDetails { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ArgumentNullException.ThrowIfNull(modelBuilder);
+
+            modelBuilder.Entity<Customer>(e =>
+            {
+                e.HasOne(c => c.Person)
+                 .WithOne()
+                 .HasForeignKey<Customer>(c => c.PersonId);
+
+                e.HasMany(c => c.Receipts)
+                 .WithOne(r => r.Customer)
+                 .HasForeignKey(r => r.CustomerId);
+            });
+
+            modelBuilder.Entity<Person>(e =>
+            {
+                e.Property(p => p.Name).IsRequired();
+                e.Property(p => p.Surname).IsRequired();
+                e.Property(p => p.BirthDate).IsRequired();
+            });
+
+            modelBuilder.Entity<Product>(e =>
+            {
+                e.HasOne(p => p.Category)
+                 .WithMany(c => c.Products)
+                 .HasForeignKey(p => p.ProductCategoryId);
+
+                e.HasMany(p => p.ReceiptDetails)
+                 .WithOne(rd => rd.Product)
+                 .HasForeignKey(rd => rd.ProductId);
+
+                e.Property(p => p.ProductName).IsRequired();
+                e.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            });
+
+            modelBuilder.Entity<ProductCategory>(e =>
+            {
+                e.Property(pc => pc.CategoryName).IsRequired();
+            });
+
+            modelBuilder.Entity<Receipt>(e =>
+            {
+                e.HasOne(r => r.Customer)
+                 .WithMany(c => c.Receipts)
+                 .HasForeignKey(r => r.CustomerId);
+
+                e.HasMany(r => r.ReceiptDetails)
+                 .WithOne(rd => rd.Receipt)
+                 .HasForeignKey(rd => rd.ReceiptId);
+
+                e.Property(r => r.OperationDate).IsRequired();
+            });
+
+            modelBuilder.Entity<ReceiptDetail>(e =>
+            {
+                e.HasOne(rd => rd.Receipt)
+                 .WithMany(r => r.ReceiptDetails)
+                 .HasForeignKey(rd => rd.ReceiptId);
+
+                e.HasOne(rd => rd.Product)
+                 .WithMany(p => p.ReceiptDetails)
+                 .HasForeignKey(rd => rd.ProductId);
+
+                e.Property(rd => rd.DiscountUnitPrice).HasColumnType("decimal(18,2)");
+                e.Property(rd => rd.UnitPrice).HasColumnType("decimal(18,2)");
+            });
+        }
     }
 }
